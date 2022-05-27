dffacd9ffa56e4c126af716b9ad2e5e3c7f4f3b1
SuperVader2015 <eyal.ktlk@gmail.com>
2022-05-27 12:49:29 +0300

    enigma machine, in progress

---
+++ app/src/main/java/com/example/enigmamachinechat/Rotor.java
@@ -0,0 +1,61 @@
+package com.example.enigmamachinechat;
+
+import android.util.Pair;
+
+import java.util.HashMap;
+
+public class Rotor {
+    private static final HashMap<Integer, Pair<String, Character>> possibleWheels =
+            new HashMap<Integer, Pair<String, Character>>() {{
+                put(1, new Pair<>("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q'));
+                put(2, new Pair<>("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E'));
+                put(3, new Pair<>("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V'));
+                put(4, new Pair<>("ESOVPZJAYQUIRHXLNFTGKDCMWB", 'J'));
+                put(5, new Pair<>("VZBRGITYUPSDNHLXAWMJQOFECK", 'Z'));
+            }};
+    private final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private String wheel;
+    private int ringSetting;
+    private int turned;
+    private char turnover;
+
+    public Rotor(int wheelNumber, int ringSetting) throws Exception {
+        if (wheelNumber < 1 || wheelNumber > 5)
+            throw new Exception("invalid wheel number");
+        this.wheel = possibleWheels.get(wheelNumber).first;
+        this.ringSetting = ringSetting % 26;
+        this.turned = -(ringSetting % 26);
+        this.turnover = (char) (((((int) (possibleWheels
+                .get(wheelNumber).second)) - 65 - this.ringSetting) + 26) % 26 + 65);
+    }
+
+    public char getTurnover() {
+        return this.turnover;
+    }
+
+    public char turn() {
+        char initial = alphabet.charAt((26 + this.turned) % 26);
+        this.turned++;
+        this.turned %= 26;
+        return initial;
+    }
+
+    public char use(char letter) throws Exception {
+        if (letter < 'A' || letter > 'Z') {
+            throw new Exception("not an uppercase letter");
+        }
+        return alphabet
+                .charAt(((((int) (this.wheel
+                        .charAt((((int) (letter)) - 65 + this.turned) % 26))) - 65 - this.turned) + 26) % 26);
+    }
+
+    public char useBackwards(char letter) throws Exception {
+        if (letter < 'A' || letter > 'Z') {
+            throw new Exception("not an uppercase letter");
+        }
+        return alphabet.charAt((this.wheel
+                .indexOf(((char) ((((int) (letter)) - 65 + this.turned) % 26 + 65))) - this.turned + 26) % 26);
+    }
+
+}
