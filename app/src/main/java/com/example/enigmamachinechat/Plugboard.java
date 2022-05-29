5c168d33decb3eac393d49f94e9fe34dc19440b6
SuperVader2015 <eyal.ktlk@gmail.com>
2022-05-29 18:05:00 +0300

    Plugboard class

---
+++ app/src/main/java/com/example/enigmamachinechat/Plugboard.java
@@ -0,0 +1,33 @@
+package com.example.enigmamachinechat;
+
+import androidx.core.util.Pair;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Plugboard {
+    private HashMap<Character, Character> plugs;
+
+    /**
+     * @param plugs pairs of connected letters
+     */
+    public Plugboard(Set<Pair<Character, Character>> plugs) {
+        this.plugs = new HashMap<Character, Character>() {{
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+                put(letter, letter);
+        }};
+        for (Pair<Character, Character> connection : plugs) {
+            this.plugs.put(connection.first, connection.second);
+            this.plugs.put(connection.second, connection.first);
+        }
+    }
+
+    /**
+     * sends a letter through the plugboard, encrypting it
+     * @param letter letter before encryption
+     * @return letter after encryption
+     */
+    public char use(char letter){
+        return this.plugs.get(letter);
+    }
+}
